8dce71f6a190e49fa2ab9f36ccfcd380b5139c1b
Brett Benda <brett.benda@ufl.edu>
2020-09-04 11:47:11 -0400

    stuff

--- js/pictures.js
+++ js/pictures.js
@@ -8,10 +8,11 @@ Promise.all([d3.json("./pictures/pic.json")]).then(
   var path = "./pictures/Photography/"
   var rowStart = "<div class=\"row row-space my-auto\">"
   var colStart = "<div class=\"col-sm-4\">"
-    for(var i=0; i<json[0].images.length; i++){
+  var j = 0;
+    for(var i=json[0].images.length-1; i>=0; i--){
         var name = json[0].images[i]
 
-        if(i%3==0)
+        if(j%3==0)
           innerHTML+=rowStart
 
         var fullpath = path+name
@@ -21,8 +22,10 @@ Promise.all([d3.json("./pictures/pic.json")]).then(
         innerHTML += "<img class=\"img-center img-thumbnail mx-auto d-block\" src=\""+fullpath+"\" style=\"max-height: 33vh; width:auto; height: auto; margin-bottom: 10px\">"
         innerHTML += "</div>"
 
-        if(i%3==2 || i==json[0].images.length-1)
+        if(j%3==2 || j==json[0].images.length-1)
           innerHTML+="</div>"
+
+      	j++
     }
     gallery.innerHTML = innerHTML
 })
